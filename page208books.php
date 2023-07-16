a87fb9240a021c320fac07892658e5c7c9c2cb31
Ezequiel Brusa <ezequielbrusa@Ezequiels-MBP.lan>
2023-07-16 01:31:36 -0400

    PHP for the web (Ullman) Chapter 8

---
+++ page208books.php
@@ -0,0 +1,18 @@
+<?php // Script 8.5 - page208books.php
+/* This page lists J.D. Salinger's bibliography. */ 
+
+// Set the page title and include the header first as CONSTANT:
+define('TITLE', 'Books by J.D. Salinger');
+include('templates/header.php');
+
+?>
+
+<h2>J.D. Salinger's Books</h2>
+<ul>
+<li>The catcher in the Rye</li>
+<li>Nine stories</li>
+<li>Franny and Zooey</li>
+<li>Raise High the Roof Beam, Carpenters and Seymour: An Introduction</li>
+</ul>
+
+<?php include('templates/footer.php'); ?>
---
+++ templates/footer.php
@@ -0,0 +1,18 @@
+<!-- END CHANGEABLE CONTENT. -->
+</main>
+
+<footer container class="siteFooter">
+<p>
+Design uses <a href="http://concisecss.com/">Concise CSS Framework</a>
+</p>
+<p class="float-right"><?php  //Print the current date and time...
+// Set the time zone:
+date_default_timezone_set('America/New_York');
+
+// Now print the date and time:
+print date('g:i a l F j');
+?></p>
+</footer>
+
+</body>
+</html>
